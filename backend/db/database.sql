f32d325e088c98c4d75d30906990454faf3b27c8
Eric Moon <moonkangjin9195@gmail.com>
2023-03-23 18:10:07 -0400

    Modify table creation in database.sql

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -1,11 +1,12 @@
 CREATE TABLE howls (
     id SERIAL NOT NULL PRIMARY KEY,
     caption VARCHAR(400),
-    createdAt timestamp with time zone
+    createdAt TIMESTAMPTZ DEFAULT current_timestamp
+    updatedAt TIMESTAMPTZ DEFAULT current_timestamp
 );
 
 -- new howl
--- INSERT INTO howls (caption, createdAt) VALUES('hello', NOW());
+-- INSERT INTO howls (caption) VALUES('hello');
 
 -- get all howls
 -- SELECT * FROM howls;
