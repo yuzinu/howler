fb387ab65f9baaec5c3ddd2c299e7d84507d7976
Eric Moon <moonkangjin9195@gmail.com>
2023-03-23 18:14:59 -0400

    Fix syntax by adding ',' and modify row names

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -1,8 +1,8 @@
 CREATE TABLE howls (
     id SERIAL NOT NULL PRIMARY KEY,
     caption VARCHAR(400),
-    createdAt TIMESTAMPTZ DEFAULT current_timestamp
-    updatedAt TIMESTAMPTZ DEFAULT current_timestamp
+    created_at TIMESTAMPTZ DEFAULT current_timestamp,
+    updated_at TIMESTAMPTZ DEFAULT current_timestamp
 );
 
 -- new howl
