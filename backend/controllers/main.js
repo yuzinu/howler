0f1e6f6c553c54a32cffca0560a9d8852e166688
Eric Moon <moonkangjin9195@gmail.com>
2023-03-27 22:43:53 -0400

    Modify backend to add get single howl

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -1,34 +1,67 @@
 const pool = require("../db/database.js");
 
-exports.getHowls = async (req, res, next) => {
-    const { rows } = await pool.query('SELECT * FROM howls');
-    
-    if(rows.length > 0) {
-        res.json(rows);
-    }else{
-        res.send("No howls found!")
-    }
-};
+module.exports = {
+    getHowls: async (req, res, next) => {
+        try {
+            const { rows } = await pool.query('SELECT * FROM howls');
+            
+            if(!rows) {
+                res.send("No howls found!");
+            }else{
+                res.json(rows);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    },
 
-exports.createHowl = async (req, res, next) => {
-    await pool.query('INSERT INTO howls (caption) VALUES($1)', [req.body.caption]);
-    
-    res.send("Howl created!");
-};
+    getHowl: async (req, res, next) => {
+        try {
+            const { rows } = await pool.query(`SELECT * FROM howls WHERE id = ${req.params.id}`);
+            
+            if(!rows) {
+                res.send("No howl found!");
+            }else{
+                const howl = rows[0];
+                res.json(howl);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    },
 
-exports.changeHowl = async (req, res, next) => {
-    const id = parseInt(req.params.id);
+    createHowl: async (req, res, next) => {
+        try {
+            await pool.query('INSERT INTO howls (caption) VALUES($1)', [req.body.caption]);
+        
+            res.send("Howl created!");
+        } catch (err) {
+            console.log(err);
+        }
+    },
 
-    await pool.query(`UPDATE howls SET caption = $1, updated_at = current_timestamp - interval '4 hours' WHERE id = $2`,
-    [req.body.caption, id]);
-    
-    res.send("Howl updated!");
-};
+    changeHowl: async (req, res, next) => {
+        try {
+            const id = parseInt(req.params.id);
 
-exports.silenceHowl = async (req, res, next) => {
-    const id = parseInt(req.params.id);
+            await pool.query(`UPDATE howls SET caption = $1, updated_at = current_timestamp - interval '4 hours' WHERE id = $2`,
+            [req.body.caption, id]);
+            
+            res.send("Howl updated!");
+        } catch (err) {
+            console.log(err);
+        }
+    },
 
-    await pool.query("DELETE FROM howls WHERE id = $1", [id]);
-    
-    res.send("Howl deleted");
+    silenceHowl: async (req, res, next) => {
+        try {
+            const id = parseInt(req.params.id);
+
+            await pool.query("DELETE FROM howls WHERE id = $1", [id]);
+            
+            res.send("Howl deleted");
+        } catch (err) {
+            console.log(err);
+        }
+    },
 };
