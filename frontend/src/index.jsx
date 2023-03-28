4c6aeda16635466ddf48111efab3bb3b7e029339
Eric Moon <moonkangjin9195@gmail.com>
2023-03-28 12:55:08 -0400

    Update react packages & configure respective files

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet,
+    ScrollRestoration,
+} from "react-router-dom";
+import Howl from './components/Howl';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const AppLayout = () => {
+    return (
+        <>
+            <ScrollRestoration />
+            <nav>
+              <ul>
+                <li></li>
+              </ul>
+            </nav>
+            <Outlet />
+        </>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        element: <AppLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/home",
+                element: <Home />,
+            },
+            {
+                path: "/howl/:howlId",
+                element: <Howl />,
+            },
+        ],
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,74 +2,73 @@ const path = require("path");
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports={
-    entry: {
-        bundle: path.resolve(__dirname, './src/index.jsx'),
-    },
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: '[name][contenthash].js',
-        clean: true,
-        assetModuleFilename: '[name][ext]',
-        
-    },
-    resolve: {
-        extensions: ['.js','.jsx','.json'] 
-    },
-    module:{
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use:  {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            },
-            { 
-                test: /\.css$/, 
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.(scss)$/,
-                use: [
-                  {
-                    loader: 'style-loader'
-                  },
-                  {
-                    loader: 'css-loader'
-                  },
-                  {
-                    loader: 'postcss-loader',
-                    options: {
-                      postcssOptions: {
-                        plugins: () => [
-                          require('autoprefixer')
-                        ]
-                      }
-                    }
-                  },
-                  {
-                    loader: 'sass-loader'
-                  }
+  entry: {
+    bundle: path.resolve(__dirname, './src/index.jsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  resolve: {
+    extensions: ['.js','.jsx','.json'] 
+  },
+  module:{
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use:  {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      { 
+        test: /\.css$/, 
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
                 ]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
         ]
-    },
-    devServer: {
-        historyApiFallback: true,
-        port: 3000,
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-          title: 'Howler',
-          filename: 'index.html',
-          template: './template.html',
-        }),
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Howler',
+      filename: 'index.html',
+      template: './template.html',
+    }),
+  ]
 }
