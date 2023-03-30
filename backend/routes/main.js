a1c4b5f18b366f67023b7122015fbd3e2483ce41
Eric Moon <moonkangjin9195@gmail.com>
2023-03-30 17:32:46 -0400

    Erase all code regarding express-openid-connect

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/main");
-const { ensureAuth } = require("../middleware/auth");
 
 router.get("/", mainController.signedIn);
 
-router.get("/api/feed", ensureAuth, mainController.getHowls);
+router.get("/api/feed", mainController.getHowls);
 
-router.get("/api/howl/:id", ensureAuth, mainController.getHowl);
+router.get("/api/howl/:id", mainController.getHowl);
 
-router.post("/api/howl/createHowl/", ensureAuth, mainController.createHowl);
+router.post("/api/howl/createHowl/", mainController.createHowl);
 
-router.patch("/api/howl/changeHowl/:id", ensureAuth, mainController.changeHowl);
+router.patch("/api/howl/changeHowl/:id", mainController.changeHowl);
 
-router.delete("/api/howl/silenceHowl/:id", ensureAuth, mainController.silenceHowl);
+router.delete("/api/howl/silenceHowl/:id", mainController.silenceHowl);
 
 module.exports = router;
