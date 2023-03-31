0dc3038ba19061cb547a593535b21fd55deaee37
Eric Moon <moonkangjin9195@gmail.com>
2023-03-31 16:55:38 -0400

    Add new user to see multiple users and their howls

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -28,3 +28,11 @@ INSERT INTO howls (caption, howler_id)
 SELECT 'Hello, are you a minion?', id
 FROM users
 WHERE auth0_token='auth0|6426dd72cff66fdcdc891d2d';
+
+INSERT INTO users (username, auth0_token)
+VALUES ('moonkangjin9195', 'google-oauth2|100942908378109046967');
+
+INSERT INTO howls (caption, howler_id)
+SELECT 'I am a minion!', id
+FROM users
+WHERE auth0_token='google-oauth2|100942908378109046967';
