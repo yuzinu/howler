2c354be6880456803ffcfc8b346cf0e4a31d661f
Eric Moon <moonkangjin9195@gmail.com>
2023-04-14 16:43:22 -0400

    Fix bug in feed and user feed by adding margin bottom

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,60 +1,58 @@
-import React, {useEffect, useState} from 'react';
-import AddHowl from './AddHowl';
-import HowlCard from './HowlCard';
+import React, { useEffect, useState } from "react";
+import AddHowl from "./AddHowl";
+import HowlCard from "./HowlCard";
 
 function Feed({ user, isAuthenticated, modalHowlSubmit, setModalHowlSubmit }) {
-    
-    const [howls, setHowls] = useState([]);
-    const [caption, setCaption] = useState("");
+  const [howls, setHowls] = useState([]);
+  const [caption, setCaption] = useState("");
 
-    if(isAuthenticated){
-        useEffect (() => {
-            fetch("https://howler-backend.onrender.com/api/feed")
-            .then(res => res.json())
-            .then(data => {
-                setHowls(data);
-                setModalHowlSubmit(false);
-            })
-            .catch(err => console.log(err));
-        }, [caption, setCaption, modalHowlSubmit, setModalHowlSubmit]);
-    } else {
-        useEffect (() => {
-            fetch("https://howler-backend.onrender.com/api/feed")
-            .then(res => res.json())
-            .then(data => {
-                setHowls(data);
-            })
-            .catch(err => console.log(err));
-        }, [caption, setCaption, modalHowlSubmit, setModalHowlSubmit]);
-    }
+  if (isAuthenticated) {
+    useEffect(() => {
+      fetch("https://howler-backend.onrender.com/api/feed")
+        .then((res) => res.json())
+        .then((data) => {
+          setHowls(data);
+          setModalHowlSubmit(false);
+        })
+        .catch((err) => console.log(err));
+    }, [caption, setCaption, modalHowlSubmit, setModalHowlSubmit]);
+  } else {
+    useEffect(() => {
+      fetch("https://howler-backend.onrender.com/api/feed")
+        .then((res) => res.json())
+        .then((data) => {
+          setHowls(data);
+        })
+        .catch((err) => console.log(err));
+    }, [caption, setCaption, modalHowlSubmit, setModalHowlSubmit]);
+  }
 
-    return (
-        <>
-            { isAuthenticated && (
-                <div className="sticky-top bg-white border-bottom">
-                    <p className='fs-6 fw-bold p-2'>Home</p>
-                    <AddHowl
-                        user={user} 
-                        isAuthenticated={isAuthenticated} 
-                        caption={caption} 
-                        setCaption={setCaption}
-                    />
-                </div>
-            )}
-            <div>
-                <ul className='p-0'>
-                    {howls.map(howl => {
-                        return (
-                            <li className="" style={{listStyle:"none"}} key={howl.id}>
-                                < HowlCard howl={howl}/>
-                            </li>
-                            
-                        );
-                    })}
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <>
+      {isAuthenticated && (
+        <div className="sticky-top bg-white border-bottom">
+          <p className="fs-6 fw-bold p-2">Home</p>
+          <AddHowl
+            user={user}
+            isAuthenticated={isAuthenticated}
+            caption={caption}
+            setCaption={setCaption}
+          />
+        </div>
+      )}
+      <div id="feed">
+        <ul className="p-0">
+          {howls.map((howl) => {
+            return (
+              <li className="" style={{ listStyle: "none" }} key={howl.id}>
+                <HowlCard howl={howl} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Feed;
--- frontend/src/components/UserFeed.jsx
+++ frontend/src/components/UserFeed.jsx
@@ -1,52 +1,51 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { useParams, useOutletContext } from "react-router-dom";
-import AddHowl from './AddHowl';
-import HowlCard from './HowlCard';
+import AddHowl from "./AddHowl";
+import HowlCard from "./HowlCard";
 
 function UserFeed() {
-    const { user, modalHowlSubmit, setModalHowlSubmit } = useOutletContext();
-    const { username } = useParams();
+  const { user, modalHowlSubmit, setModalHowlSubmit } = useOutletContext();
+  const { username } = useParams();
 
-    const [userHowls, setUserHowls] = useState([]);
-    const [caption, setCaption] = useState("");
+  const [userHowls, setUserHowls] = useState([]);
+  const [caption, setCaption] = useState("");
 
-    useEffect (() => {
-        fetch(`https://howler-backend.onrender.com/api/${username}`)
-        .then(res => res.json())
-        .then(data => {
-            setUserHowls(data);
-            setModalHowlSubmit(false);
-        })
-        .catch(err => console.log(err));
-    }, [caption, setCaption, modalHowlSubmit, setModalHowlSubmit]);
+  useEffect(() => {
+    fetch(`https://howler-backend.onrender.com/api/${username}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUserHowls(data);
+        setModalHowlSubmit(false);
+      })
+      .catch((err) => console.log(err));
+  }, [caption, setCaption, modalHowlSubmit, setModalHowlSubmit]);
 
-    if (user) {
-        return (
-            <>
-                <div className="sticky-top bg-white border-bottom">
-                    <h2 className='fs-6 fw-bold px-2 pt-2 m-0'>{user.name}</h2>
-                    <small className="text-muted px-2 pb-2 m-0">{userHowls.length > 0 ? userHowls.length : "0"} Howls</small>
-                    <AddHowl
-                        caption={caption} 
-                        setCaption={setCaption}
-                    />
-                </div>
-                <div>
-                    <ul className='p-0'>
-                        {userHowls.map(userHowl => {
-                            return (
-                                <li style={{listStyle:"none"}} key={userHowl.id}>
-                                    < HowlCard userHowl={userHowl}/>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            </>
-        )
-    } else {
-        return
-    }
+  if (user) {
+    return (
+      <>
+        <div className="sticky-top bg-white border-bottom">
+          <h2 className="fs-6 fw-bold px-2 pt-2 m-0">{user.name}</h2>
+          <small className="text-muted px-2 pb-2 m-0">
+            {userHowls.length > 0 ? userHowls.length : "0"} Howls
+          </small>
+          <AddHowl caption={caption} setCaption={setCaption} />
+        </div>
+        <div id="feed">
+          <ul className="p-0">
+            {userHowls.map((userHowl) => {
+              return (
+                <li style={{ listStyle: "none" }} key={userHowl.id}>
+                  <HowlCard userHowl={userHowl} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </>
+    );
+  } else {
+    return;
+  }
 }
 
 export default UserFeed;
