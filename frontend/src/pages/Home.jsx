7a7455a47b6b6c2ce627ba92c8445d076141d39f
Eric Moon <moonkangjin9195@gmail.com>
2023-04-28 23:44:45 -0400

    Make modifications to see if new users are entered into db after sign up

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,40 +1,62 @@
-import { useOutletContext } from 'react-router-dom';
-import React from "react";
+import React, { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
 import Feed from "../components/Feed";
 
 function Home() {
-    const { isAuthenticated, user, isLoading, modalHowlSubmit, setModalHowlSubmit } = useOutletContext();
-    
-    if (isAuthenticated) {
-    
-        if (isLoading) {
-          return <div>Loading ...</div>;
-        };
+  const {
+    isAuthenticated,
+    user,
+    isLoading,
+    modalHowlSubmit,
+    setModalHowlSubmit,
+  } = useOutletContext();
 
-        return (
-            <div className='container px-0'>
-                <div className='w-100'>
-                    <Feed 
-                        user={user} 
-                        isAuthenticated={isAuthenticated} 
-                        modalHowlSubmit={modalHowlSubmit} 
-                        setModalHowlSubmit={setModalHowlSubmit}
-                    />
-                </div>
-            </div>
-        )
-    } else {
-    
-        return (
-            <div className='container px-0'>
-                <div className='w-100'>
-                    <Feed 
-                        isAuthenticated={isAuthenticated} 
-                    />
-                </div>
-            </div>
-        )
+  useEffect(() => {
+    if (isAuthenticated) {
+      const body = {
+        username: user.nickname,
+        auth0_token: user.sub,
+      };
+
+      fetch("https://howler-backend.onrender.com/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          window.alert(data.message);
+        })
+        .catch((err) => console.log(err));
     }
+  }, []);
+
+  if (isAuthenticated) {
+    if (isLoading) {
+      return <div>Loading ...</div>;
+    }
+
+    return (
+      <div className="container px-0">
+        <div className="w-100">
+          <Feed
+            user={user}
+            isAuthenticated={isAuthenticated}
+            modalHowlSubmit={modalHowlSubmit}
+            setModalHowlSubmit={setModalHowlSubmit}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container px-0">
+        <div className="w-100">
+          <Feed isAuthenticated={isAuthenticated} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
